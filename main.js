c735db4dc595e7e18e05acb7121e19f9697f791f
nikoladev <nikola@pilipovic.nl>
2019-03-04 14:42:34 +0100

    get rid of tabs

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ const application = require("application")
 const fs = require("uxp").storage.localFileSystem
 
 const removeTheNotApproved = (unsanitized) => {
-	return unsanitized.replace(/[\\:*?"<>|#]/g, '')
+  return unsanitized.replace(/[\\:*?"<>|#]/g, '')
 }
 
 const dashesForSlashes = (unsanitized) => {
-	return unsanitized.replace(/\//g, '-')
+  return unsanitized.replace(/\//g, '-')
 }
 
 /**
@@ -16,7 +16,7 @@ const dashesForSlashes = (unsanitized) => {
  * Dashed: /
  */
 const sanitizeName = (unsanitized) => {
-	return removeTheNotApproved(dashesForSlashes(unsanitized))
+  return removeTheNotApproved(dashesForSlashes(unsanitized))
 }
 
 async function exportAt(selection, scale) {
@@ -24,20 +24,20 @@ async function exportAt(selection, scale) {
   const folder = await fs.getFolder()
 
   await Promise.all(selection.items.map(async (item) => {
-  	try {
-  		const file = await folder.createFile(`${sanitizeName(item.name)}.png`, {overwrite: true})
-	  	renditionSettings.push({
-		    node: item,
-		    outputFile: file,
-		    type: application.RenditionType.PNG,
-		    scale: 3,
-			})
-  	} catch (e) {
-  		console.log(e)
-  	}
+    try {
+      const file = await folder.createFile(`${sanitizeName(item.name)}.png`, {overwrite: true})
+      renditionSettings.push({
+        node: item,
+        outputFile: file,
+        type: application.RenditionType.PNG,
+        scale: 3,
+      })
+    } catch (e) {
+      console.log(e)
+    }
   }))
 
-	application.createRenditions(renditionSettings)
+  application.createRenditions(renditionSettings)
     .then(results => {
       console.log('All are done')
     })
@@ -47,83 +47,83 @@ async function exportAt(selection, scale) {
 }
 
 function exportDialog() {
-	/**
-	 * Height and width need to be set up front. Can't them change later
-	 *
-	 * On Windows the size of dialog is constrained by the size of the window
-	 */
+  /**
+   * Height and width need to be set up front. Can't them change later
+   *
+   * On Windows the size of dialog is constrained by the size of the window
+   */
   const dialog = document.createElement("dialog")
 
   dialog.innerHTML = `
-		<form method="dialog">
+    <form method="dialog">
       <div>Which scale do you want to export at?</div>
-	    <footer>
+      <footer>
         <button
-        	id="cancel"
-        	type="reset"
-        	uxp-variant="secondary"
-        	uxp-quiet="true"
-      	>Cancel</button>
-
-        <button
-        	id="1x"
-        	type="button"
-        	uxp-variant="primary"
-      	>1x</button>
+          id="cancel"
+          type="reset"
+          uxp-variant="secondary"
+          uxp-quiet="true"
+        >Cancel</button>
 
         <button
-        	id="2x"
-        	type="button"
-        	uxp-variant="primary"
-      	>2x</button>
+          id="1x"
+          type="button"
+          uxp-variant="primary"
+        >1x</button>
 
         <button
-        	id="3x"
-        	type="button"
-        	uxp-variant="primary"
-        	autofocus="autofocus"
-      	>3x</button>
-	    </footer>
-		</form>
-	`
+          id="2x"
+          type="button"
+          uxp-variant="primary"
+        >2x</button>
 
-	let responseValue
-	const closeOptions = {
-		'#cancel': 'reasonCanceled',
-		'#1x': 1,
-		'#2x': 2,
-		'#3x': 3,
-	}
+        <button
+          id="3x"
+          type="button"
+          uxp-variant="primary"
+          autofocus="autofocus"
+        >3x</button>
+      </footer>
+    </form>
+  `
 
-	// Listening to the 'close' event is the only way I can set the dialog
-	// response when ENTER is pressed (otherwise it's just an empty string)
-	dialog.addEventListener('close', (evt) => {
+  let responseValue
+  const closeOptions = {
+    '#cancel': 'reasonCanceled',
+    '#1x': 1,
+    '#2x': 2,
+    '#3x': 3,
+  }
+
+  // Listening to the 'close' event is the only way I can set the dialog
+  // response when ENTER is pressed (otherwise it's just an empty string)
+  dialog.addEventListener('close', (evt) => {
     dialog.close(responseValue)
-	})
+  })
 
-	Object.keys(closeOptions).forEach((key) => {
-		// Clicking on a button will prepare the correct response value and then
-		// directly close the dialog
-		dialog.querySelector(key).addEventListener('click', () => {
-	    responseValue = closeOptions[key]
-	    dialog.close()
-		})
+  Object.keys(closeOptions).forEach((key) => {
+    // Clicking on a button will prepare the correct response value and then
+    // directly close the dialog
+    dialog.querySelector(key).addEventListener('click', () => {
+      responseValue = closeOptions[key]
+      dialog.close()
+    })
 
-		// Focusing on a button (like when TABbing through the buttons) will prepare
-		// the correct response value when closing the dialog
-		dialog.querySelector(key).addEventListener('focus', (evt) => {
-			responseValue = closeOptions[key]
-		})
-	})
+    // Focusing on a button (like when TABbing through the buttons) will prepare
+    // the correct response value when closing the dialog
+    dialog.querySelector(key).addEventListener('focus', (evt) => {
+      responseValue = closeOptions[key]
+    })
+  })
 
-	// The only downside to the current approach is that the ESC key doesn't work
-	// as expected. This event listener fixes that.
-	dialog.addEventListener('keydown', (evt) => {
-		// capture if ESC key is pressed and set the appropriate response value
-		if (evt.keyCode === 27) {
-			responseValue = 'reasonCanceled'
-		}
-	})
+  // The only downside to the current approach is that the ESC key doesn't work
+  // as expected. This event listener fixes that.
+  dialog.addEventListener('keydown', (evt) => {
+    // capture if ESC key is pressed and set the appropriate response value
+    if (evt.keyCode === 27) {
+      responseValue = 'reasonCanceled'
+    }
+  })
 
   document.appendChild(dialog)
 
@@ -134,29 +134,29 @@ function noSelectionDialog () {
   const dialog = document.createElement("dialog")
 
   dialog.innerHTML = `
-		<form method="dialog">
-			<h2>⚠️ Not so fast</h2>
+    <form method="dialog">
+      <h2>⚠️ Not so fast</h2>
       <p>Don't forget to select something to export :)</p>
 
-	    <footer>
+      <footer>
         <button
-        	id="ok"
-        	type="submit"
-        	uxp-variant="cta"
-      	>OK, I will select something</button>
-	    </footer>
-		</form>
-	`
-	document.appendChild(dialog)
+          id="ok"
+          type="submit"
+          uxp-variant="cta"
+        >OK, I will select something</button>
+      </footer>
+    </form>
+  `
+  document.appendChild(dialog)
 
   return dialog
 }
 
 async function showDialog(selection) {
   if (!selection.items.length) {
-  	const dialog = noSelectionDialog()
-  	await dialog.showModal()
-  	dialog.remove()
+    const dialog = noSelectionDialog()
+    await dialog.showModal()
+    dialog.remove()
     return
   }
 
@@ -169,11 +169,11 @@ async function showDialog(selection) {
 
   if (response !== 'reasonCanceled' && typeof response === 'number') {
     exportAt(selection, response)
-	}
+  }
 
-	// If the dialog is not explicitly removed it will stay in RAM but won't be
-	// visible to the user.
-	dialog.remove()
+  // If the dialog is not explicitly removed it will stay in RAM but won't be
+  // visible to the user.
+  dialog.remove()
 }
 
 module.exports = {
